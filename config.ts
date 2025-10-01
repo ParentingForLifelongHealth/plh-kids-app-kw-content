5da78457b6cc23eb2c32bc732f5c971552e298dd
EsmeeIDEMS <esmee@idems.international>
2025-10-01 16:07:41 +0200

    content: 1.4.5

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ const config = extendDeploymentConfig({ name: "plh_kids_kw", parent: "plh_kids" 
 
 config.git = {
   content_repo: "https://github.com/ParentingForLifelongHealth/plh-kids-app-kw-content",
-  content_tag_latest: "1.4.3",
+  content_tag_latest: "1.4.5",
 };
 
 config.android = {
